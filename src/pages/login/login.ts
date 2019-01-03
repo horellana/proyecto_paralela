1eec87120741a77cd7bcac1303990fea0c4e0269
Hector Orellana <hector.orellanaf92@gmail.com>
2019-01-03 00:18:28 -0300

    muestro error cuando falta rut o contraseña

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,12 +3,29 @@ import { NavController } from 'ionic-angular';
 import { RecuperarContraseAPage } from '../recuperar-contrase-a/recuperar-contrase-a';
 import { PrincipalPage } from '../principal/principal';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+    loginForm: FormBuilder;
 
+    constructor(public navCtrl: NavController,
+                public formBuilder: FormBuilder,
+        this.loginForm = this.formBuilder.group({
+            rut: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+    login(params) {
+        if (this.loginForm.status === "INVALID") {
+            this.alertCtrl
+                .create({title: 'Error',
+                         subTitle: 'Por favor ingresa todos los datos'})
+                .present();
+            return;
+        }
     goToRecuperarContraseA(params){
         if (!params) params = {};
         this.navCtrl.setRoot(RecuperarContraseAPage);
