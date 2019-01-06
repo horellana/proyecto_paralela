7f946e5d34ebbff7cdeb7dc5cca2da727a3011c9
Hector Orellana <hector.orellanaf92@gmail.com>
2019-01-06 00:14:24 -0300

    fix imports

--- src/pages/recuperar-contrase-a/recuperar-contrase-a.ts
+++ src/pages/recuperar-contrase-a/recuperar-contrase-a.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { LoginPage } from '../login/login';
-import { HomeEstudiantePage } from '../home-estudiante/home-estudiante';
 import { AlertController } from 'ionic-angular';
 
 
